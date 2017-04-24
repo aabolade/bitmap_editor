49656a6d366b8a6367ddef85abb7a039bd6f3ff4
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-04-24 23:36:50 +0100

    returns message for unrecognised command

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -4,8 +4,14 @@ describe BitmapEditor do
 
   let(:editor) {described_class.new}
   let(:show) {"examples/show.txt"}
+  let(:unrecognised) {"examples/unrecognised.txt"}
+
   it "displays a message when there is no image" do
     expect(editor.output(show)).to eq "There is no image"
+  end
+
+  it "returns a message for an unrecognised command" do
+    expect(editor.output(unrecognised)).to eq "unrecognised command :("
   end
 
 end
