2b7ae0e0ac8aee53c661dda605efcdf401d98155
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-04-25 13:16:22 +0100

    creating a new image with all pixels coloured O

--- lib/image.rb
+++ lib/image.rb
@@ -1,11 +1,18 @@
 class Image
 
   attr_reader :width, :height
+  attr_accessor :pixels
 
   def initialize(width, height)
     @width = width
     @height = height
+    @pixels = []
   end
 
+  def create_pixels
+    row = []
+    width.times {row << "O"}
+    height.times {pixels << row}
+  end
 
 end
--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -1,9 +1,10 @@
 require 'image'
 
 describe Image do
-  let(:width) {double("width")}
-  let(:height) {double("height")}
+  let(:width) {3}
+  let(:height) {2}
   let(:image) {described_class.new(width, height)}
+  let(:pixel_image) {[["O","O","O"],["O","O","O"]]}
 
   it "has a width" do
     expect(image.width).to eq width
@@ -11,6 +12,29 @@ describe Image do
 
   it "has a height" do
     expect(image.height).to eq height
+  end
+
+  it "has an array of pixels" do
+    expect(image.pixels).to eq []
+  end
+
+  describe "creating pixels" do
+    before do
+      image.create_pixels
+    end
+
+    it "making a two dimension array" do
+      expect(image.pixels).to eq pixel_image
+    end
+
+    it "size matches the image height" do
+      expect(image.pixels.count).to eq height
+    end
+
+    it "size matches the image width" do
+      expect(image.pixels.first.count).to eq width
+    end
+
   end
 
 end
